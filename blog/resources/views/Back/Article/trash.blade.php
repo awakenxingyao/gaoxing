719e23505df90398627609da673768672e02fecc
awakenxingyao <33280079+awakenxingyao@users.noreply.github.com>
2017-11-01 18:46:46 +0800

    Update trash.blade.php

--- blog/resources/views/Back/Article/trash.blade.php
+++ blog/resources/views/Back/Article/trash.blade.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by 小韩说理
- * User: 韩忠康
- * Date: 2017/9/21
- * Time: 14:33
- */
+
 ?>
 @extends('Back.layout')
 @section('right')
@@ -73,7 +68,7 @@
 
         <section class="content-footer">
             <div class="text-center">
-                &copy;HelloKang，程序员
+                &copy;HelloGao，太极养生
             </div>
         </section><!-- /.content-footer -->
 
