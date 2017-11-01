60441430defa2bbf590a25e0b00b0d995d0598a0
awakenxingyao <33280079+awakenxingyao@users.noreply.github.com>
2017-11-01 18:57:01 +0800

    Update footer.blade.php

--- blog/resources/views/Back/footer.blade.php
+++ blog/resources/views/Back/footer.blade.php
@@ -1,13 +1,8 @@
 <?php
-/**
- * Created by 小韩说理
- * User: 韩忠康
- * Date: 2017/9/18
- * Time: 17:36
- */
+
 ?>
 <section class="content-footer">
     <div class="text-center">
-        &copy;HelloKang，<a href="http://www.hellokang.net">小韩说理</a>
+        &copy;HelloKang，<a href="http://www.junong18.com">高兴说理</a>
     </div>
 </section><!-- /.content-footer -->
